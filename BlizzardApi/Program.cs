a63c041b898bf09362603d46e3953475c62b87d5
Kennith Mann <kmann@etherpunk.com>
2021-04-27 15:45:23 -0500

    Add project files.

---
+++ BlizzardApi/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using BlizzardApi.WoW;
+using BlizzardApi.WoW.GameData;
+using System.Configuration;
+
+namespace BlizzardApi
+{
+    class Program
+    {
+        public static void Main()
+        {
+            string clientId = ConfigurationManager.AppSettings["ClientId"];
+            string secret = ConfigurationManager.AppSettings["Secret"];
+            string accessToken = Base.GetAuthToken(WoW.Base.Region.US, clientId, secret).Result;
+
+            var x = MythicKeystoneDungeonApi.GetMythicKeystoneDungeon(accessToken, Base.Region.US, Base.Locale.EN_US).Result;
+
+            Console.WriteLine(x.ToString());
+            Console.Read();
+        }
+    }
+}
